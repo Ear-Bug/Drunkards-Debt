12a2e1d6302b16a22fd86863f73f3444d016cc1c
Ear-Bug <61468141+Ear-Bug@users.noreply.github.com>
2024-10-15 15:27:33 -0700

    Added Table, Dice, and Cup

---
+++ Assets/Scripts/Main Menu.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        onButtonClicked();
+    }
+
+    // Click the "Instantiate!" button and a new `prefab` will be instantiated
+    // somewhere within -10.0 and 10.0 (inclusive) on the x-z plane
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 50), "Instantiate!"))
+        {
+            var position = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
+            Instantiate(prefab, position, Quaternion.identity);
+        }
+    }
+    private void onButtonClicked()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("Button Clicked");
+
+            //gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/MouseInputs.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInputs : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Left Click
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse 0 - Left Click");
+
+        }
+        //Right Click
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Mouse 1 - Right Click");
+
+        }
+        //Middle Mouse Click
+        if (Input.GetMouseButtonDown(2))
+        {
+            Debug.Log("Mouse 2 - Middle Mouse Click");
+
+        }
+    }
+}
